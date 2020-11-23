4961eaedc16a42cdde6f6e5040c7b040ceb88636
Adam Seewald <ads@mmmi.sdu.dk>
2020-11-23 18:32:19 +0100

    fixes to the lemma but still more fixes to come

--- scripts/lemma.m
+++ scripts/lemma.m
@@ -2,28 +2,34 @@
 
 % using this script to temporarily proof the lemma
 
-a0 = 10;
-a0 = qt(1);
-a1 = 1.43;
-a1 = qt(2);
+a0 = 2000;
+%a0 = qt(1);
+a1 = 200;
+%a1 = qt(2);
 
-b1 = 1.43;
-b1 = qt(3);
+b1 = 200;
+%b1 = qt(3);
+
+a2 = 200;
+b2 = 200;
+
+a3 = 200;
+b3 = 200;
 
 T = 100;
 
 o = 2*pi/T;
 
-h = @(k) a0/T + 2*(a1*cosd(o*k) + b1*sind(o*k))/T; 
-               % 2*(a2*cosd(o*2*k) + b2*sind(o*2*k))/T + ...
-                %2*(a3*cosd(o*3*k) + b3*sind(o*3*k))/T;
+h = @(k) a0/T + 2*(a1*cos(o*k) + b1*sin(o*k))/T + ...
+                2*(a2*cos(o*2*k) + b2*sin(o*2*k))/T + ...
+                2*(a3*cos(o*3*k) + b3*sin(o*3*k))/T;
               
 c = @(k) a0/T + (exp(1i*o*k)*(a1 - 1i*b1))/T + ...
-                (exp(-1i*o*k)*(a1 + 1i*b1))/T;
-                %(exp(1i*o*2*k)*(a2 - 1i*b2))/T + ...
-                %(exp(-1i*o*2*k)*(a2 + 1i*b2))/T + ...
-                %(exp(1i*o*3*k)*(a3 - 1i*b3))/T + ...
-                %(exp(-1i*o*3*k)*(a3 + 1i*b3))/T;
+                (exp(-1i*o*k)*(a1 + 1i*b1))/T + ...
+                (exp(1i*o*2*k)*(a2 - 1i*b2))/T + ...
+                (exp(-1i*o*2*k)*(a2 + 1i*b2))/T + ...
+                (exp(1i*o*3*k)*(a3 - 1i*b3))/T + ...
+                (exp(-1i*o*3*k)*(a3 + 1i*b3))/T;
               
 x = 1:1000;
 y = h(x);
@@ -31,38 +37,42 @@ y = h(x);
 figure;
 plot(x, y);
 
-Aj = @(j) [ 1 o*j ; -o*j 1 ];
-A  = zeros(3);
-A(1,1) = 1;
+Aj = @(j) [ 0 o*j ; -o*j 0 ];
+A  = zeros(7);
+A(1,1) = 0;
 C = 1;
 
-for i = 1:1
+for i = 1:3
     A(2 * i : 2 * i + 1, 2 * i : 2 * i + 1) = Aj(i); 
-    C = [C 1 1];
+    C = [C 1 0];
 end
 
-q0 = [a0; a1; b1];    
+q0 = [a0; 2*a1; 2*b1; 2*a2; 2*b2; 2*a3; 2*b3]; 
 C = 1/T * C;
 
 yy = [];
     
 q = [];
+
+%A(2,3) = 0;
+%A(3,2) = 0;
+%A(3,3) = A(3,2);
+
+for k = 1:.01:x(end)
+    q1 = (eye(size(q0,1)) + A*.01)*q0;
     
-for k = x
-    q1 = A * q0;
     y1 = C * q1;
-        
+    
     yy = [yy y1];
     q = [q q1];
-        
-    q0 = q1;    
+
+    q0 = q1;
 end
 
 figure;
-plot(x, yy);
+plot(1:.01:x(end), yy);
 
 yyy = c(x);
-
 
 figure;
 plot(x, yyy);
